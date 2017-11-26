ce4b5a4a5388f399d8f91a875485191a8fcc6918
Andy Leverenz <andy@justalever.com>
2017-11-26 14:46:18 -0600

    update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	# before_action :find_post, only: [:show, :update, :edit, :destroy]
+	 # before_action :find_post, only: [:show, :update, :edit, :destroy]
 
 	def index
 		@posts = Post.all.order("created_at DESC")
@@ -20,11 +20,9 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 
 		if @post.update(post_params)
 			redirect_to @post
