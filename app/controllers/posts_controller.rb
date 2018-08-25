f6aa7d8f3ff5837d267dd58bd78c84b3e468bc49
Reetesh Singh <reetesh.singh422@gmail.com>
2018-08-25 16:43:47 +0530

    edit : show and update methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,10 +20,11 @@ class PostsController < ApplicationController
 	end
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def update
-
+		@post = Post.find(params[:id])
 		if @post.update(post_params)
 			redirect_to @post
 		else
